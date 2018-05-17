b775cd3e15eb22772e2324ee4a509fb1fcce212a
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-05-16 20:48:52 -0500

    improved displaying list of grades

--- grades/main.js
+++ grades/main.js
@@ -1,16 +1,13 @@
 const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60, 77, 74, 73]
 const grades = {
-    "A": 0, 
-    "B": 0, 
+    "A": 0,
+    "B": 0,
     "C": 0,
-    "D": 0, 
+    "D": 0,
     "F": 0
 }
 
-
 // Displaying grades from the array 
-// How many of each grade? Accomplish this with a for..in loop. 
-
 for (let i = 0; i < scores.length; i++) {
     if (scores[i] >= 91) {
         grades.A++
@@ -20,18 +17,22 @@ for (let i = 0; i < scores.length; i++) {
         grades.C++
     } else if (scores[i] >= 61 && scores[i] < 71) {
         grades.D++
-    } else if (scores[i] <=60) {
+    } else if (scores[i] <= 60) {
         grades.F++
     }
 }
-// console.log(grades)
+
+// How many of each grade? Accomplish this with a for..in loop. 
+for (let key in grades) {
+    console.log(`There are ${grades[key]} "${key}" grades`);
+}
 
 // What is the lowest score? Sort the array and find out.
 // What is the highest score?
 for (let i = 0; i < scores.length; i++) {
-    scores.sort(function(a, b){return a - b});
+    scores.sort(function (a, b) { return a - b });
     // console.log(scores[i]);
-    }
+}
 
 // Which grade had the most students achieve it? Use an if statment, and a currentGradeCount variable, in your for..in loop to see if the current grade's value is higher than the last one.
 // Which grade had the fewest students achieve it?
@@ -46,7 +47,7 @@ for (key in grades) {
     let currentGradeCount = grades[key];
 
     // checks if the current key in the loop is greater than the numOfGrades value
-    if(currentGradeCount > mostNumOfGrades) {
+    if (currentGradeCount > mostNumOfGrades) {
         // if true, updates mostNumOfGrades value to the updated number
         mostNumOfGrades = currentGradeCount;
         // and updates the letter grade highestGradeCount variable
@@ -62,8 +63,8 @@ let lowestGradeCount;
 
 for (key in grades) {
     let currentGradeCount = grades[key];
-    
-    if(currentGradeCount < leastNumOfGrades) {
+
+    if (currentGradeCount < leastNumOfGrades) {
         leastNumOfGrades = currentGradeCount;
         lowestGradeCount = key;
     }
